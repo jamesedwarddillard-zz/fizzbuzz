406f22ad973a04864b6ccbba3daeaf7fa0c4d1db
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-07-26 12:55:15 -0400

    checked for divisibility by 3 and 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,3 +7,28 @@ Requirements:
 a) replace numbers divisible by 3 with fizz
 b) replace numbers divisible by 5 with buzz
 c) replace numbers divisible by 3 and 5 with FizzBuzz"""
+
+def fizzbuzz(n):
+	Print "FizzBuzz counting up to %s" % str(n)
+
+	for i in n:
+
+		if i % 3 == 0:
+
+			if i % 5 == 0:
+
+				print 'Fizz Buzz'
+
+			else:
+
+				print 'Fizz'
+
+		elif i % 5 == 0:
+
+			print 'Buzz'
+
+		else:
+
+			print i 
+
+fizzbuzz(100)
